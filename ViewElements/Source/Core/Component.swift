656424d7bd9cf029524d36ff5dfbede2ae257b87
aunnnn <aun.wirawit@gmail.com>
2017-06-10 15:11:09 +0700

    Support scrollable horizontal stack

--- ViewElements/Source/Core/Component.swift
+++ ViewElements/Source/Core/Component.swift
@@ -55,6 +55,31 @@ private class ComponentOfStackView: ComponentOf<StackProps> {
     }
 }
 
+/// Internal scroll view used to wrap stack view to make it scrollable.
+internal class _ScrollView: UIScrollView, ElementDisplayable {
+    var element: ElementOfView?
+    
+    static func buildMethod() -> ViewBuildMethod {
+        return .init
+    }
+    
+    func setup() {
+        self.showsHorizontalScrollIndicator = false
+        self.showsVerticalScrollIndicator = false
+        self.clipsToBounds = false
+    }
+    
+    func update() {
+        if let subview = self.subviews.first as? ElementDisplayable {
+            subview.element = self.element
+            subview.update()
+        } else {
+            fatalError("No subview of _ScrollView found.")
+        }
+    }
+}
+
+/// Internal stack view used to build Component.
 internal class _StackView: UIStackView, ElementDisplayable {
     
     var element: ElementOfView?
@@ -214,10 +239,34 @@ public final class VerticalStack: StackProps {
 }
 
 public final class HorizontalStack: StackProps {
+    
+    /// Whether the stack view is scrollable when the content is larger than a stack space. 
+    /// If true, the stack view will be wrapped inside a scroll view.
+    public var scrollable = false
+    
     public init(distribute: UIStackViewDistribution, align: UIStackViewAlignment, spacing: CGFloat, _ elements: @escaping @autoclosure () -> [ViewBuildable]) {
         super.init(.horizontal, elements)
         self.distribution = distribute
         self.alignment = align
         self.spacing = spacing
+    }
+    
+    public override func build() -> UIView {
+        let stackView = super.build() as! UIStackView
+        if scrollable {
+            let scrollView = _ScrollView()
+            scrollView.setup()
+            scrollView.addSubview(stackView)
+            stackView.al_pinToEdges(ofView: scrollView)
+            stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+            return scrollView
+        } else {
+            return stackView
+        }
+    }
+    
+    public func scrollable(_ value: Bool) -> HorizontalStack {
+        self.scrollable = value
+        return self
     }
 }

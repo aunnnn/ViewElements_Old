ca0ef31a5c2c97fb417c6b89b4701a4ed73a812d
aunnnn <aun.wirawit@gmail.com>
2017-12-06 22:45:19 +0700

    Fix new api string

--- ViewElements/Source/Utilities.swift
+++ ViewElements/Source/Utilities.swift
@@ -10,7 +10,7 @@ import Foundation
 
 func randomAlphaNumericString(length: Int) -> String {
     let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let allowedCharsCount = UInt32(allowedChars.characters.count)
+    let allowedCharsCount = UInt32(allowedChars.count)
     var randomString = ""
     
     for _ in 0..<length {
